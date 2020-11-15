5451e6001e850269f757376d4917b044c2309fbc
Steven Weingärtner <pd@frozen-bits.de>
2020-11-15 16:19:53 +0100

    feat: handle kanka mention links
    External links to kanka are fixed. If an entity was synced to
    foundry the link will now open the journal entry instead.

---
+++ src/hooks/renderJournalSheet.ts
@@ -0,0 +1,40 @@
+import Campaign from '../kanka/Campaign';
+import moduleConfig from '../module.json';
+import { findEntryByEntityId } from '../module/journal';
+
+interface Options {
+    entity: JournalEntry['data'],
+}
+
+export default async function renderJournalSheet(
+    sheet: JournalSheet,
+    html: JQuery,
+    options?: Options,
+): Promise<void> {
+    html.on('click', '[data-id]', (event: JQuery.ClickEvent) => {
+        const entityId = Number(event.currentTarget.dataset.id);
+        const entry = findEntryByEntityId(entityId);
+
+        if (entry) {
+            event.stopImmediatePropagation();
+            event.preventDefault();
+
+            entry.sheet.render(true);
+        }
+    });
+
+    const { entity } = options ?? {};
+    const campaign = game.modules.get(moduleConfig.name).campaign as Campaign;
+
+    if (entity?.flags[moduleConfig.name]) {
+        const contentElement = html.find('.editor-content');
+
+        // Workaround for missing locale in links
+        const parsedContent = contentElement.html().replace(
+            'https://kanka.io/campaign',
+            `https://kanka.io/${campaign.locale}/campaign`,
+        );
+
+        contentElement.html(parsedContent);
+    }
+}
--- src/kanka-foundry.ts
+++ src/kanka-foundry.ts
@@ -3,12 +3,15 @@ import hmrWrapHook from './hooks/hmrWrapHook';
 import init from './hooks/init';
 import renderJournalDirectory from './hooks/renderJournalDirectory';
 import './kanka-foundry.scss';
+import renderJournalSheet from './hooks/renderJournalSheet';
 
 const refreshInit = hmrWrapHook('init', () => init, 'once');
 const refreshRenderJournalDirectory = hmrWrapHook('renderJournalDirectory', () => renderJournalDirectory, 'on');
+const refreshRenderJournalSheet = hmrWrapHook('renderJournalSheet', () => renderJournalSheet, 'on');
 
 if (module.hot) {
     CONFIG.debug.hooks = true;
     module.hot.accept('./hooks/init', refreshInit);
     module.hot.accept('./hooks/renderJournalDirectory', refreshRenderJournalDirectory);
+    module.hot.accept('./hooks/renderJournalSheet', refreshRenderJournalSheet);
 }
