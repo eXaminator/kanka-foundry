d53ad842944444a211d6dac389d36a83e281d828
Steven Weingärtner <pd@frozen-bits.de>
2021-06-15 20:50:19 +0200

    feat: show specific list of journal entries and folders that will be migrated on module update
    This should make situations easier where not all entries can be updated and you are not sure why.

--- src/migrations/migrateV1.ts
+++ src/migrations/migrateV1.ts
@@ -1,5 +1,6 @@
 import type KankaFoundry from '../KankaFoundry';
 import { KankaApiEntity } from '../types/kanka';
+import createJournalLink from '../util/createJournalLink';
 
 async function migrateAll(module: KankaFoundry): Promise<void> {
     const campaignId = module.currentCampaign?.id;
@@ -61,6 +62,38 @@ async function migrateAll(module: KankaFoundry): Promise<void> {
     module.showInfo('migration.success', { success: successCount, expected: updateEntities.length });
 }
 
+function createEntryList(label: string, entries: JournalEntry[]): string {
+    if (entries.length === 0) return '';
+
+    const listItems = entries
+        .map(entry => createJournalLink(entry))
+        .join(', ');
+
+    return `<details><summary><strong>${label}</strong></summary><p>${listItems}</p></details>`;
+}
+
+function createFolderList(label: string, folders: Folder[]): string {
+    if (folders.length === 0) return '';
+
+    const listItems = folders
+        .map((folder) => {
+            const path = [folder];
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            while ((path[0] as any).parentFolder) {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                path.unshift((path[0] as any).parentFolder);
+            }
+
+            return path
+                .map((folder, idx) => (idx === path.length - 1 ? `<strong>${folder.name}</strong>` : folder.name))
+                .join(' › ');
+        })
+        .map(path => `<li>${path}</li>`)
+        .join('');
+
+    return `<details><summary><strong>${label}</strong></summary><ul>${listItems}</ul></details>`;
+}
+
 export default function migrateV1(module: KankaFoundry): void {
     const outdatedJournals = game.journal.filter(e => e.getFlag(module.name, 'id') && !e.getFlag(module.name, 'snapshot'));
     const outdatedFolders = game.folders.filter(folder => folder.getFlag(module.name, 'folderType'));
@@ -75,6 +108,14 @@ export default function migrateV1(module: KankaFoundry): void {
             documentCount: outdatedJournals.length,
             folderCount: outdatedFolders.length,
             campaignName: module.currentCampaign?.name,
+            journalList: createEntryList(
+                module.getMessage('migration.dialog.listHeader.journals', { count: outdatedJournals.length }),
+                outdatedJournals,
+            ),
+            folderList: createFolderList(
+                module.getMessage('migration.dialog.listHeader.folders', { count: outdatedFolders.length }),
+                outdatedFolders,
+            ),
         }),
         buttons: {
             yes: {
