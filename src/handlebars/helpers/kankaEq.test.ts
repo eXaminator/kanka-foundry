8b86f8b589affe67d3a18a05ca8af6658263cc00
Steven Weingärtner <pd@frozen-bits.de>
2021-06-26 17:57:01 +0200

    feat: improve asset gallery view

--- src/handlebars/helpers/kankaEq.test.ts
+++ src/handlebars/helpers/kankaEq.test.ts
@@ -25,14 +25,26 @@ describe('kankaEq()', () => {
         expect(compile(template)).toEqual('success');
     });
 
-    it('returns false if both values strictly equal and the not-option was set', () => {
+    it('returns false if both values strictly equal and the not-option is set', () => {
         const template = '{{#unless (kankaEq 0 0 not=true)}}success{{/unless}}';
 
         expect(compile(template)).toEqual('success');
     });
 
-    it('returns true if both values are different and the not-option was set', () => {
+    it('returns true if both values are different and the not-option is set', () => {
         const template = '{{#if (kankaEq 0 "0" not=true)}}success{{/if}}';
+
+        expect(compile(template)).toEqual('success');
+    });
+
+    it('returns true if first value matches regex of second value', () => {
+        const template = '{{#if (kankaEq "foobar" "regex:^foo")}}success{{/if}}';
+
+        expect(compile(template)).toEqual('success');
+    });
+
+    it('returns false if first value matches regex of second value and the not-option is set', () => {
+        const template = '{{#unless (kankaEq "foobar" "regex:^foo" not=true)}}success{{/unless}}';
 
         expect(compile(template)).toEqual('success');
     });
