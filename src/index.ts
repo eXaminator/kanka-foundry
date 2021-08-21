5d8d92d1c70304fc993bae68d954cc57f27e127a
akrigline <a.krigline@gmail.com>
2021-08-20 20:21:42 -0400

    fix: activate JournalEntry settings for newest documentSheetRegistrar versions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import deleteJournalEntry from './hooks/deleteJournalEntry';
 import hmrWrapHook from './hooks/hmrWrapHook';
 import init from './hooks/init';
+import preDocumentSheetRegistrarInit from './hooks/preDocumentSheetRegistrarInit';
 import ready from './hooks/ready';
 import renderJournalDirectory from './hooks/renderJournalDirectory';
 import renderSettingsConfig from './hooks/renderSettingsConfig';
 import './index.scss';
 import './KankaJournal/KankaJournalApplication';
 
+const refreshPreDocumentSheetRegistrarInit = hmrWrapHook('preDocumentSheetRegistrarInit', () => preDocumentSheetRegistrarInit, 'once');
 const refreshInit = hmrWrapHook('init', () => init, 'once');
 const refreshReady = hmrWrapHook('ready', () => ready, 'once');
 const refreshRenderJournalDirectory = hmrWrapHook('renderJournalDirectory', () => renderJournalDirectory, 'on');
@@ -14,6 +16,7 @@ const refreshRenderSettingsConfig = hmrWrapHook('renderSettingsConfig', () => re
 const refreshDeleteJournalSheet = hmrWrapHook('deleteJournalEntry', () => deleteJournalEntry, 'on');
 
 if (module.hot) {
+    module.hot.accept('./hooks/preDocumentSheetRegistrarInit', refreshPreDocumentSheetRegistrarInit);
     module.hot.accept('./hooks/init', refreshInit);
     module.hot.accept('./hooks/ready', refreshReady);
     module.hot.accept('./hooks/renderJournalDirectory', refreshRenderJournalDirectory);
