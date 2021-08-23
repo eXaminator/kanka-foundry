89e14a094e531e04a6b02e64b480107b2788e9c9
Steven Weingärtner <pd@frozen-bits.de>
2021-08-23 19:37:05 +0200

    fix: ensure document-sheet-registrar is active on ready

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import documentSheetRegistrarInit from './hooks/documentSheetRegistrarInit';
 import hmrWrapHook from './hooks/hmrWrapHook';
 import init from './hooks/init';
 import preDocumentSheetRegistrarInit from './hooks/preDocumentSheetRegistrarInit';
+import ready from './hooks/ready';
 import renderJournalDirectory from './hooks/renderJournalDirectory';
 import renderSettingsConfig from './hooks/renderSettingsConfig';
 import './index.scss';
 import './KankaJournal/KankaJournalApplication';
 
 const refreshInit = hmrWrapHook('init', () => init, 'once');
+const refreshReady = hmrWrapHook('ready', () => ready, 'once');
 const refreshPreDocumentSheetRegistrarInit = hmrWrapHook('preDocumentSheetRegistrarInit', () => preDocumentSheetRegistrarInit, 'once');
 const refreshDocumentSheetRegistrarInit = hmrWrapHook('documentSheetRegistrarInit', () => documentSheetRegistrarInit, 'once');
 const refreshRenderJournalDirectory = hmrWrapHook('renderJournalDirectory', () => renderJournalDirectory, 'on');
@@ -17,6 +19,7 @@ const refreshDeleteJournalSheet = hmrWrapHook('deleteJournalEntry', () => delete
 
 if (module.hot) {
     module.hot.accept('./hooks/init', refreshInit);
+    module.hot.accept('./hooks/ready', refreshReady);
     module.hot.accept('./hooks/preDocumentSheetRegistrarInit', refreshPreDocumentSheetRegistrarInit);
     module.hot.accept('./hooks/documentSheetRegistrarInit', refreshDocumentSheetRegistrarInit);
     module.hot.accept('./hooks/renderJournalDirectory', refreshRenderJournalDirectory);
