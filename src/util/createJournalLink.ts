e511d25ff4fe142a8a2cb092871b3c55a879be06
Joseph Henrich <crimsonknave@gmail.com>
2021-12-27 23:09:51 -0500

    Updates for v9

--- src/util/createJournalLink.ts
+++ src/util/createJournalLink.ts
@@ -1,7 +1,7 @@
 export default function createJournalLink(
     entry: JournalEntry,
     label?: string,
-    classes = 'entity-link',
+    classes = 'entity-link content-link',
 ): string {
-    return `<a draggable="true" data-entity="JournalEntry" data-id="${entry.id}" class="${classes}"><i class="fas fa-book-open"></i> ${label ?? entry.name}</a>`;
+    return `<a draggable="true" data-entity="JournalEntry" data-tyep="JournalEntry" data-id="${entry.id}" class="${classes}"><i class="fas fa-book-open"></i> ${label ?? entry.name}</a>`;
 }
