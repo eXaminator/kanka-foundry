6631836b6bbb5c9d24120923889de705a7bd6648
Steven Weingärtner <pd@frozen-bits.de>
2022-01-07 12:27:35 +0100

    fix: update handling of reference links to work in FVTT 9 and onwards
    Fixes #85

--- src/util/createJournalLink.ts
+++ src/util/createJournalLink.ts
@@ -1,7 +1,6 @@
 export default function createJournalLink(
     entry: JournalEntry,
     label?: string,
-    classes = 'entity-link',
 ): string {
-    return `<a draggable="true" data-entity="JournalEntry" data-id="${entry.id}" class="${classes}"><i class="fas fa-book-open"></i> ${label ?? entry.name}</a>`;
+    return TextEditor.enrichHTML(`@JournalEntry[${entry.id}]{${label ?? entry.name}}`);
 }
