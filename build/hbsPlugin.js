d64d84bc431d5261eea59bf3aaf9b0878ece9e66
Steven Weingärtner <pd@frozen-bits.de>
2022-10-09 10:21:21 +0200

    build: fix hbs HMR

--- build/hbsPlugin.js
+++ build/hbsPlugin.js
@@ -10,6 +10,56 @@ export default function hbsPlugin() {
     let config;
     let server;
 
+    async function load(id) {
+        if (!id.endsWith('.hbs')) {
+            return null;
+        }
+
+        const inputBasePath = dirname(config.build.lib.entry);
+        const inputRelativePath = relative(inputBasePath, id);
+        const outputRelativePath = join('templates', inputRelativePath);
+        const outputPath = join(config.build.outDir, outputRelativePath);
+        const outputUrl = join(config.base, outputRelativePath).replace(/^\//, '');
+        const content = readFileSync(id).toString('utf-8');
+
+        const partialRegex = /\{\{(#|~)?>(\.[-a-zA-Z0-9/_.]+)([^}]*)}}/g;
+        const matches = Array.from(content.matchAll(partialRegex));
+        const partials = matches.map(([, , partialPath]) => getPartialPath(id, partialPath));
+
+        const partialImports = partials
+            .map((partial) => `import ${JSON.stringify(partial)};`)
+            .join('\n');
+
+        const parsedContent = matches.reduce((content, match) => {
+            if (!match[2]) return content;
+            const partialPath = relative(inputBasePath, getPartialPath(id, match[2]));
+            const partialUrl = join(config.base, 'templates', partialPath).replace(/^\//, '');
+            return content.replaceAll(
+                new RegExp(`${match[2]}(}| |\n)`, 'g'),
+                (all, ending) => `${partialUrl}${ending}`,
+            );
+        }, content);
+
+        if (server) {
+            mkdirSync(dirname(outputPath), { recursive: true });
+            writeFileSync(outputPath, parsedContent);
+        } else {
+            this.emitFile({
+                type: 'asset',
+                fileName: outputRelativePath,
+                source: parsedContent,
+            });
+        }
+
+        const fullTemplateUrl = JSON.stringify(outputUrl);
+
+        return `
+        ${partialImports};
+        Hooks.once('init', () => loadTemplates([${fullTemplateUrl}]));
+        export default ${fullTemplateUrl};
+        `;
+    }
+
     return {
         name: 'hbs-plugin',
         configResolved(resolvedConfig) {
@@ -18,55 +68,7 @@ export default function hbsPlugin() {
         configureServer(_server) {
             server = _server;
         },
-        async load(id) {
-            if (!id.endsWith('.hbs')) {
-                return null;
-            }
-
-            const inputBasePath = dirname(config.build.lib.entry);
-            const inputRelativePath = relative(inputBasePath, id);
-            const outputRelativePath = join('templates', inputRelativePath);
-            const outputPath = join(config.build.outDir, outputRelativePath);
-            const outputUrl = join(config.base, outputRelativePath).replace(/^\//, '');
-            const content = readFileSync(id).toString('utf-8');
-
-            const partialRegex = /\{\{(#|~)?>(\.[-a-zA-Z0-9/_.]+)([^}]*)}}/g;
-            const matches = Array.from(content.matchAll(partialRegex));
-            const partials = matches.map(([, , partialPath]) => getPartialPath(id, partialPath));
-
-            const partialImports = partials
-                .map((partial) => `import ${JSON.stringify(partial)};`)
-                .join('\n');
-
-            const parsedContent = matches.reduce((content, match) => {
-                if (!match[2]) return content;
-                const partialPath = relative(inputBasePath, getPartialPath(id, match[2]));
-                const partialUrl = join(config.base, 'templates', partialPath).replace(/^\//, '');
-                return content.replaceAll(
-                    new RegExp(`${match[2]}(}| |\n)`, 'g'),
-                    (all, ending) => `${partialUrl}${ending}`,
-                );
-            }, content);
-
-            if (server) {
-                mkdirSync(dirname(outputPath), { recursive: true });
-                writeFileSync(outputPath, parsedContent);
-            } else {
-                this.emitFile({
-                    type: 'asset',
-                    fileName: outputRelativePath,
-                    source: parsedContent,
-                });
-            }
-
-            const fullTemplateUrl = JSON.stringify(outputUrl);
-
-            return `
-            ${partialImports};
-            Hooks.once('init', () => loadTemplates([${fullTemplateUrl}]));
-            export default ${fullTemplateUrl};
-            `;
-        },
+        load,
         async handleHotUpdate({ server, file, modules }) {
             // Send event to frontend which can then remove the file from the cache and rerender open apps
             if (!file.endsWith('.hbs')) {
@@ -74,8 +76,8 @@ export default function hbsPlugin() {
             }
 
             const promises = modules.map(async (module) => {
-                await this.load(module.id);
-
+                await load(module.id);
+                
                 const inputBasePath = dirname(config.build.lib.entry);
                 const inputRelativePath = relative(inputBasePath, module.file);
                 const url = join(config.base, 'templates', inputRelativePath).replace(/^\//, '');
