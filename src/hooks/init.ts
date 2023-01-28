1e4517d93aada98cbc3516038c9c58ff5b5fce8f
Steven Weingärtner <pd@frozen-bits.de>
2023-01-28 18:04:53 +0100

    refactor: make handlebars helpers hot reloadable

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,11 +1,23 @@
 import kanka from '../kanka';
+import registerHandlebarsHelpers from '../module/registerHandlebarsHelper';
 
 export default async function init(): Promise<void> {
+    registerHandlebarsHelpers();
     await kanka.initialize();
 }
 
 if (import.meta.hot) {
     import.meta.hot.dispose(async () => {
         await kanka.dispose();
+    });
+
+    import.meta.hot.accept((newModule) => {
+        if ((game as Game).ready) {
+            newModule?.default();
+
+            Object
+                .values(ui.windows)
+                .forEach(app => app.render(false));
+        }
     });
 }
