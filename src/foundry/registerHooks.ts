a52d96f968a9ba2d288627d6634d8cfcbe2d06c0
Steven Weingärtner <sweingaertner@brainbits.net>
2023-05-29 10:38:18 +0200

    fix: move loading of campaign futher back to prevent certain initializations from beeing blocked if it fails

--- src/foundry/registerHooks.ts
+++ src/foundry/registerHooks.ts
@@ -3,14 +3,17 @@ import hmrWrapHook from './hooks/hmrWrapHook';
 import init from './hooks/init';
 import renderJournalDirectory from './hooks/renderJournalDirectory';
 import renderSettingsConfig from './hooks/renderSettingsConfig';
+import setup from './hooks/setup';
 
 const refreshInit = hmrWrapHook('init', () => init, 'once');
+const refreshSetup = hmrWrapHook('setup', () => setup, 'once');
 const refreshRenderJournalDirectory = hmrWrapHook('renderJournalDirectory', () => renderJournalDirectory, 'on');
 const refreshRenderSettingsConfig = hmrWrapHook('renderSettingsConfig', () => renderSettingsConfig, 'on');
 const refreshDeleteJournalSheet = hmrWrapHook('deleteJournalEntry', () => deleteJournalEntry, 'on');
 
 if (import.meta.hot) {
     import.meta.hot.accept('./hooks/init', refreshInit);
+    import.meta.hot.accept('./hooks/setup', refreshSetup);
     import.meta.hot.accept('./hooks/renderJournalDirectory', refreshRenderJournalDirectory);
     import.meta.hot.accept('./hooks/renderSettingsConfig', refreshRenderSettingsConfig);
     import.meta.hot.accept('./hooks/deleteJournalEntry', refreshDeleteJournalSheet);
