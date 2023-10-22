43a3ecde3cdec98392ee671448015b5170bdaf88
Steven Weingärtner <sweingaertner@brainbits.net>
2023-10-22 13:58:22 +0200

    fix: move certain setup code and migrations to the ready hook

--- src/foundry/registerHooks.ts
+++ src/foundry/registerHooks.ts
@@ -3,17 +3,17 @@ import hmrWrapHook from './hooks/hmrWrapHook';
 import init from './hooks/init';
 import renderJournalDirectory from './hooks/renderJournalDirectory';
 import renderSettingsConfig from './hooks/renderSettingsConfig';
-import setup from './hooks/setup';
+import ready from './hooks/ready';
 
 const refreshInit = hmrWrapHook('init', () => init, 'once');
-const refreshSetup = hmrWrapHook('setup', () => setup, 'once');
+const refreshReady = hmrWrapHook('ready', () => ready, 'once');
 const refreshRenderJournalDirectory = hmrWrapHook('renderJournalDirectory', () => renderJournalDirectory, 'on');
 const refreshRenderSettingsConfig = hmrWrapHook('renderSettingsConfig', () => renderSettingsConfig, 'on');
 const refreshDeleteJournalSheet = hmrWrapHook('deleteJournalEntry', () => deleteJournalEntry, 'on');
 
 if (import.meta.hot) {
     import.meta.hot.accept('./hooks/init', refreshInit);
-    import.meta.hot.accept('./hooks/setup', refreshSetup);
+    import.meta.hot.accept('./hooks/ready', refreshReady);
     import.meta.hot.accept('./hooks/renderJournalDirectory', refreshRenderJournalDirectory);
     import.meta.hot.accept('./hooks/renderSettingsConfig', refreshRenderSettingsConfig);
     import.meta.hot.accept('./hooks/deleteJournalEntry', refreshDeleteJournalSheet);
--- src/migrations/2021-08-23-permissions.ts
+++ src/migrations/2021-08-23-permissions.ts
@@ -3,6 +3,11 @@ import { getSetting, setSetting } from '../foundry/settings';
 export default async function migrate(): Promise<void> {
     const permissionSetting = getSetting('automaticPermissions') as string;
 
+    // Don't run the migration if the setting is already a valid value
+    if (['never', 'initial', 'always'].includes(permissionSetting)) {
+        return;
+    }
+
     if (permissionSetting === 'false') {
         await setSetting('automaticPermissions', 'never');
     } else if (permissionSetting === 'true') {
