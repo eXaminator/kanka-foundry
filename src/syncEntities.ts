2e5acc9ef9032a7494466b3cb5a8c2b674798de2
Steven Weingärtner <sweingaertner@brainbits.net>
2024-05-04 17:20:06 +0200

    fix: reduce the number of requests when syncing multiple entities at once

--- src/syncEntities.ts
+++ src/syncEntities.ts
@@ -1,7 +1,18 @@
 /* eslint-disable no-await-in-loop, no-restricted-syntax */
 import loaders from './api/typeLoaders';
 import { createJournalEntry, updateJournalEntry } from './foundry/journalEntries';
-import { KankaApiEntity, KankaApiEntityType, KankaApiId } from './types/kanka';
+import { KankaApiChildEntity, KankaApiEntity, KankaApiEntityType, KankaApiId } from './types/kanka';
+import AbstractTypeLoader from './api/typeLoaders/AbstractTypeLoader';
+
+async function handleEntity(
+    loader: AbstractTypeLoader,
+    entity: KankaApiChildEntity,
+    campaignId: KankaApiId,
+    entityLookup: KankaApiEntity[] = [],
+) {
+    const references = await loader.createReferenceCollection(campaignId, entity, entityLookup);
+    await createJournalEntry(campaignId, loader.getType(), entity, references);
+}
 
 export async function createEntity(
     campaignId: KankaApiId,
@@ -9,12 +20,12 @@ export async function createEntity(
     id: KankaApiId,
     entityLookup: KankaApiEntity[] = [],
 ): Promise<void> {
+    console.log('CREATE ENTITY', campaignId, type, id);
     const loader = loaders.get(type);
     if (!loader) throw new Error(`Missing loader for type ${String(type)}`);
     const entity = await loader.load(campaignId, id);
 
-    const references = await loader.createReferenceCollection(campaignId, entity, entityLookup);
-    await createJournalEntry(campaignId, type, entity, references);
+    await handleEntity(loader, entity, campaignId, entityLookup);
 }
 
 export async function createEntities(
@@ -23,7 +34,24 @@ export async function createEntities(
     ids: KankaApiId[],
     entityLookup: KankaApiEntity[] = [],
 ): Promise<void> {
-    await Promise.all(ids.map(id => createEntity(campaignId, type, id, entityLookup)));
+    const numberOfEntities = entityLookup.filter(entity => entity.type === type).length;
+    const expectedNumberRequests = Math.ceil(numberOfEntities / 45);
+
+    // Check whether fetching all entities of the type would be more efficient than fetching them individually
+    if (ids.length > expectedNumberRequests) {
+        const loader = loaders.get(type);
+        if (!loader) throw new Error(`Missing loader for type ${String(type)}`);
+
+        const entities = await loader.loadAll(campaignId);
+        entities.filter(entity => ids.includes(entity.id));
+
+        for (const entity of entities) {
+            // Make sure to handle them in sequence to avoid duplicate folders being created
+            await handleEntity(loader, entity, campaignId, entityLookup);
+        }
+    } else {
+        await Promise.all(ids.map(id => createEntity(campaignId, type, id, entityLookup)));
+    }
 }
 
 export async function updateEntity(entry: JournalEntry, entityLookup: KankaApiEntity[] = []): Promise<void> {
